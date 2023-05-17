5efb0d5bb6855ac5c0ed7f2b1191c9c6385bc690
angelooz <angelo-mjz7@hotmail.com>
2023-05-17 09:13:58 -0500

    chore: update

--- src/Controllers/conexionController.ts
+++ src/Controllers/conexionController.ts
@@ -1,7 +1,7 @@
 import { Socket } from 'socket.io';
 
 export function configureConexionController(socket: Socket) {
-    console.log(`Socket connected: ${socket.id}`);
+    // console.log(`Socket connected: ${socket.id}`);
 
     // socket.emit('connection', 'Hola mundo desde el server');
     
